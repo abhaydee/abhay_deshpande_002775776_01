04ed81b366e505d78e776b7c7c4889e3bffdbe7b
abhaydee <abhaygirish1997@gmail.com>
2024-01-29 18:48:25 -0500

    changes for body check

--- server.js
+++ server.js
@@ -1,61 +1,62 @@
-let express = require('express');
+let express = require("express");
 
 let app = express();
 const nocache = require("nocache");
 
-app.use(nocache())
+app.use(nocache());
 let databaseStatus = 503;
 
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
-const databaseConnection = async()=>{
-    const pool = new Pool({
-        user:"abhaydeshpande",
-        host: "localhost",
-        database:"cloudusers",
-        password:"abhaydeshpande",
-        port : 5432
-    })
+const databaseConnection = async () => {
+  const pool = new Pool({
+    user: "abhaydeshpande",
+    host: "localhost",
+    database: "cloudusers",
+    password: "abhaydeshpande",
+    port: 5432,
+  });
 
-    pool.on('error', (err, client) => {
-        console.log('postgres connection error : ' + err)
-        
-    })
-   
-    try { 
-        const client = await pool.connect()
-        databaseStatus = 200
-        client.release();
-    }
-    catch(err){
-        console.log("hitting again")
-        console.log("database connection error", err)
-        databaseStatus = 503
-    }
- 
-}
+  pool.on("error", (err, client) => {
+    console.log("postgres connection error : " + err);
+  });
 
-app.get("/healthz", async function(req, res){
-     await databaseConnection();
-    res.sendStatus(databaseStatus);
-})
+  try {
+    const client = await pool.connect();
+    databaseStatus = 200;
+    client.release();
+  } catch (err) {
+    console.log("hitting again");
+    console.log("database connection error", err);
+    databaseStatus = 503;
+  }
+};
 
-app.put("/healthz", function(req, res){
-    res.sendStatus(405)
-})
-
-app.delete("/healthz", function(req, res){
-    res.sendStatus(405)
-})
-
-app.patch("/healthz", function(req, res){
+app.get("/healthz", async function (req, res) {
+  if (Object.keys(req.body).length > 0) {
     res.sendStatus(405);
-})
+  } else {
+    await databaseConnection();
+    res.sendStatus(databaseStatus);
+  }
+});
 
-app.post("/healthz", function(req, res){
-    res.sendStatus(405)
-})
+app.put("/healthz", function (req, res) {
+  res.sendStatus(405);
+});
 
-let server = app.listen(8080,  function(){
-    console.log("the app is running on the port 8080")
-})
+app.delete("/healthz", function (req, res) {
+  res.sendStatus(405);
+});
+
+app.patch("/healthz", function (req, res) {
+  res.sendStatus(405);
+});
+
+app.post("/healthz", function (req, res) {
+  res.sendStatus(405);
+});
+
+let server = app.listen(8080, function () {
+  console.log("the app is running on the port 8080");
+});
