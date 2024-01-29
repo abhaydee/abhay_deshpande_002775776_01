44a7f6d22398e8b3fc44d5d1b350299d9fc14833
abhaydee <abhaygirish1997@gmail.com>
2024-01-29 16:39:57 -0500

    Changes for the database connection check

---
+++ server.js
@@ -0,0 +1,61 @@
+let express = require('express');
+
+let app = express();
+const nocache = require("nocache");
+
+app.use(nocache())
+let databaseStatus = 503;
+
+const {Pool} = require("pg");
+
+const databaseConnection = async()=>{
+    const pool = new Pool({
+        user:"abhaydeshpande",
+        host: "localhost",
+        database:"cloudusers",
+        password:"abhaydeshpande",
+        port : 5432
+    })
+
+    pool.on('error', (err, client) => {
+        console.log('postgres connection error : ' + err)
+        
+    })
+   
+    try { 
+        const client = await pool.connect()
+        databaseStatus = 200
+        client.release();
+    }
+    catch(err){
+        console.log("hitting again")
+        console.log("database connection error", err)
+        databaseStatus = 503
+    }
+ 
+}
+
+app.get("/healthz", async function(req, res){
+     await databaseConnection();
+    res.sendStatus(databaseStatus);
+})
+
+app.put("/healthz", function(req, res){
+    res.sendStatus(405)
+})
+
+app.delete("/healthz", function(req, res){
+    res.sendStatus(405)
+})
+
+app.patch("/healthz", function(req, res){
+    res.sendStatus(405);
+})
+
+app.post("/healthz", function(req, res){
+    res.sendStatus(405)
+})
+
+let server = app.listen(8080,  function(){
+    console.log("the app is running on the port 8080")
+})
